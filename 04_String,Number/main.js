7c7604459e2c5724f61d99f3deedda44ae452985
JaehyoJJang <yshrim12@naver.com>
2023-03-05 16:32:51 +0900

    [ADD]: String , Number 연산

---
+++ 04_String,Number/main.js
@@ -0,0 +1,23 @@
+// 더하기
+console.log(1 + 2);
+
+// 빼기
+console.log(3 - 1);
+
+// 나누기
+console.log(10 / 2);
+
+// 문자열 + 숫자 : 문자열로 변환되어 반환(return) 됨.
+console.log('a' + 1); // a1
+
+// 문자열 숫자 + 숫자 
+console.log('1' + 1); // 11
+
+// 숫자 + 빈 값(공백)
+console.log(1 + ''); // String 1
+
+// 문자열 * 숫자
+console.log('a' * 1); // NaN(Not a Number)
+
+// 문자열 숫자 * 숫자
+console.log('2' * 2); // 4
