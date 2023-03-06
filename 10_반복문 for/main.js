fca8a6184d0eaec5252efcf0b80a02fcfe5f67b5
JaehyoJJang <yshrim12@naver.com>
2023-03-06 18:29:24 +0900

    [ADD]: 반복문 for

---
+++ 10_반복문 for/main.js
@@ -0,0 +1,18 @@
+{
+    // for #1
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+{
+    // for #2
+    loopCount = 1;
+    for (;loopCount <= 10;) {
+        // 반복 동작 수행
+        console.log(loopCount);
+
+        // i 증감
+        loopCount += 1
+    }
+}
