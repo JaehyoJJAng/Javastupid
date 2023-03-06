c17a91e72d183c085a587805c887977423114d34
JaehyoJJang <yshrim12@naver.com>
2023-03-06 16:53:32 +0900

    [UPD]: 자바스크립트 코드 - 파일로 분리

--- 09_D-Day-Counter 기능 만들기/js/function.js
+++ 09_D-Day-Counter 기능 만들기/js/function.js
@@ -1,26 +1,80 @@
-// 함수 정의
-const consoleTool = function () {
-    console.log('consoleTool Function');
+const dateFormMaker = function () {
+    // input year
+    const inputYear = document.querySelector('#target-year-input').value
+
+    // input month
+    const inputMonth = document.querySelector('#target-month-input').value
+
+    // input day
+    const inputDay = document.querySelector('#target-day-input').value
+
+    // Date Format
+    // 템플릿 리터럴 방식 : (`${Variable}`)
+    return `${inputYear}-${inputMonth}-${inputDay}`
 }
 
-// 함수 호출
-consoleTool()
+// Date 함수 응용
+const countMaker = function () {
+    // DateFormat 받아오기
+    const dateFormat = dateFormMaker();
 
+    // 현재 날짜 추출
+    const nowDate = new Date();
 
-// 함수의 선언 방식 #1
-function sum(a,b) {
-    return a + b;
+    // 특정 날짜 추출
+    const targetDate = new Date(dateFormat);
+
+    // D-day 계산
+    const remaining = (targetDate - nowDate) / 1000;
+    
+    // container 태그 찍어보기
+    // console.log(container);
+    // console.log(newContainer);
+
+    if (remaining <= 0) {
+        // 타이머가 끝난 경우
+        console.log('타이머 종료');
+        container.style.display = 'none';
+        newContainer.innerHTML = `<h3>타이머가 종료되었습니다.</h3>`;
+        newContainer.style.display = 'flex';
+        return; // 함수 종료
+        
+        // 입력 값 결과가 NaN 인 경우
+        // 입력 값 중 하나가 스트링인 경우 연산 값 오류 등 ..
+    } else if (isNaN(remaining)) {
+        console.log('유효하지 않은 시간대');
+        container.style.display = 'none';
+        newContainer.innerHTML = `<h3>유효하지 않은 시간대</h3>`;
+        newContainer.style.display = 'flex';
+        return // 함수 종료
+    }
+
+    // 남은 날짜 계산
+    const remainingDate = Math.floor((remaining / 3600) / 24);
+
+    // 남은 시간 계산
+    const remainingHours = Math.floor((remaining / 3600) % 24);
+
+    // 남은 분 계산
+    const remainingMin = Math.floor((remaining / 60) % 60);
+
+    // 남은 초 계산
+    const remainingSec = Math.floor(remaining % 60);
+    
+    // 서식
+    //const dDayText = `${dateFormat} 까지 ${remainingDate} 일 ${remainingHours} 시간 ${remainingMin} 분 남았습니다`
+
+    // 날짜 넣을 태그 가져오기 : <span id='days'>0</span>
+    const days = document.getElementById('days');
+    const hour = document.getElementById('hour');
+    const min = document.getElementById('min');
+    const sec = document.getElementById('sec');
+
+    // 해당 태그들에 값 넣기
+    days.textContent = remainingDate;
+    hour.textContent = remainingHours;
+    min.textContent = remainingMin;
+    sec.textContent = remainingSec;            
+    
+    // console.log(remainingDate, remainingHours, remainingMin, remainingSec);
 }
-sum(a=5,b=5);
-
-// 함수의 선언 방식 #2
-sum(a=5,b=5); 
-const sum = function(a,b) {
-    return a + b;
-}
-
-// 함수의 선언 방식 #3 : 화살표 함수
-const sum = (a,b) => {
-    return a + b;
-}
-sum(a=5,b=5);
