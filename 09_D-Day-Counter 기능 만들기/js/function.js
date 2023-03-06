f6c7294252757d331da0640faf0e3d2c0313dcbf
JaehyoJJang <yshrim12@naver.com>
2023-03-06 12:52:17 +0900

    [UPD]: 함수의 선언 종류

--- 09_D-Day-Counter 기능 만들기/js/function.js
+++ 09_D-Day-Counter 기능 만들기/js/function.js
@@ -5,3 +5,22 @@ const consoleTool = function () {
 
 // 함수 호출
 consoleTool()
+
+
+// 함수의 선언 방식 #1
+function sum(a,b) {
+    return a + b;
+}
+sum(a=5,b=5);
+
+// 함수의 선언 방식 #2
+sum(a=5,b=5); 
+const sum = function(a,b) {
+    return a + b;
+}
+
+// 함수의 선언 방식 #3 : 화살표 함수
+const sum = (a,b) => {
+    return a + b;
+}
+sum(a=5,b=5);
